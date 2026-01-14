c872dce839cce5fecf0a6e76db36acf34f87f1b1
small-frappuccino <QdmyEep2DQMA5f@gmail.com>
2026-01-14 06:00:58 -0300

    feat: add custom Discord Rich Presence support with configuration management

--- pkg/util/application.go
+++ pkg/util/application.go
@@ -27,7 +27,7 @@ var (
 )
 
 // DiscordCoreVersion is the current version of the discordcore package.
-const DiscordCoreVersion = "v0.165.0"
+const DiscordCoreVersion = "v0.166.0"
 
 // AppVersion is the version of the application using discordcore.
 var AppVersion string
