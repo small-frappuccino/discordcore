505d78033c32f5866bd44f10961a4d7a23086a3c
small-frappuccino <QdmyEep2DQMA5f@gmail.com>
2026-01-20 06:58:47 -0300

    chore: bump discordcore to v0.207.0

--- pkg/util/application.go
+++ pkg/util/application.go
@@ -27,7 +27,7 @@ var (
 )
 
 // DiscordCoreVersion is the current version of the discordcore package.
-const DiscordCoreVersion = "v0.207.0"
+const DiscordCoreVersion = "v0.208.0"
 
 // AppVersion is the version of the application using discordcore.
 var AppVersion string
