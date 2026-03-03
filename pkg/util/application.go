c1f5d7f423af3fb85bb22f820b6a14e6a3d7c098
small-frappuccino <QdmyEep2DQMA5f@gmail.com>
2026-03-03 14:52:54 -0300

    refactor: update .gitignore and add VSCode settings for improved tooling supportrefactor: update .gitignore and add VSCode settings for improved tooling support

--- pkg/util/application.go
+++ pkg/util/application.go
@@ -27,7 +27,7 @@ var (
 )
 
 // DiscordCoreVersion is the current version of the discordcore package.
-const DiscordCoreVersion = "v0.233.0"
+const DiscordCoreVersion = "v0.234.0"
 
 // AppVersion is the version of the application using discordcore.
 var AppVersion string
