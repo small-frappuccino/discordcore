6576d0baa7e439d197c90eabfccf514b4ff1a380
small-frappuccino <QdmyEep2DQMA5f@gmail.com>
2026-03-04 19:08:45 -0300

    fix: minor fixes

--- pkg/util/application.go
+++ pkg/util/application.go
@@ -27,7 +27,7 @@ var (
 )
 
 // DiscordCoreVersion is the current version of the discordcore package.
-const DiscordCoreVersion = "v0.239.0"
+const DiscordCoreVersion = "v0.240.0"
 
 // AppVersion is the version of the application using discordcore.
 var AppVersion string
