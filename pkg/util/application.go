9b7820fc1a0b0f2ba45a7acb8d1fe9ff09c02d60
small-frappuccino <QdmyEep2DQMA5f@gmail.com>
2026-04-14 20:28:58 -0300

    fix: fix sql bug

--- pkg/util/application.go
+++ pkg/util/application.go
@@ -27,7 +27,7 @@ var (
 )
 
 // DiscordCoreVersion is the current version of the discordcore package.
-const DiscordCoreVersion = "v0.380.0"
+const DiscordCoreVersion = "v0.381.0"
 
 // AppVersion is the version of the application using discordcore.
 var AppVersion string
