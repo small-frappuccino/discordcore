1da220f6034ad6e7a0a9e444d4573c78dc7915ee
small-frappuccino <QdmyEep2DQMA5f@gmail.com>
2026-05-06 23:58:33 -0300

    feat(cmd): add discordmaindev and discordqotddev test binaries using development tokens

---
+++ cmd/discordmaindev/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"io"
+	"log/slog"
+	"os"
+
+	runtimecmd "github.com/small-frappuccino/discordcore/cmd/internal/runtimecmd"
+	discordcoreapp "github.com/small-frappuccino/discordcore/pkg/app"
+)
+
+var runDiscordMainDev = discordcoreapp.RunWithOptions
+
+func main() {
+	if err := run(os.Args[1:], os.Stderr); err != nil {
+		slog.Error("Fatal", "err", err)
+		os.Exit(1)
+	}
+}
+
+func run(args []string, output io.Writer) error {
+	return runtimecmd.Run(append([]string{"-testing"}, args...), output, runtimecmd.MainSpec("discordmaindev"), runDiscordMainDev)
+}
--- pkg/util/application.go
+++ pkg/util/application.go
@@ -27,7 +27,7 @@ var (
 )
 
 // DiscordCoreVersion is the current version of the discordcore package.
-const DiscordCoreVersion = "v0.488.1"
+const DiscordCoreVersion = "v0.489.0-rc.1"
 
 // AppVersion is the version of the application using discordcore.
 var AppVersion string
