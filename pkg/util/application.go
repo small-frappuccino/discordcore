ae3a31fcdca39515ae1f9d25c549ee3a7702acde
small-frappuccino <QdmyEep2DQMA5f@proton.me>
2026-05-10 13:48:03 -0300

    fix: remove .idea directory from .gitignore to allow IDE configuration files

--- pkg/util/application.go
+++ pkg/util/application.go
@@ -27,7 +27,7 @@ var (
 )
 
 // DiscordCoreVersion is the current version of the discordcore package.
-const DiscordCoreVersion = "v0.495.0-rc.1"
+const DiscordCoreVersion = "v0.495.0"
 
 // AppVersion is the version of the application using discordcore.
 var AppVersion string
