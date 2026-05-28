5b44f58c5196226502590190357a606f4bb51cf9
small-frappuccino <QdmyEep2DQMA5f@proton.me>
2026-05-28 11:34:29 -0300

    refactor(commands): decompose moderation commands

--- pkg/util/application.go
+++ pkg/util/application.go
@@ -27,7 +27,7 @@ var (
 )
 
 // DiscordCoreVersion is the current version of the discordcore package.
-const DiscordCoreVersion = "v0.529.0-rc.8"
+const DiscordCoreVersion = "v0.529.0-rc.9"
 
 // AppVersion is the version of the application using discordcore.
 var AppVersion string
