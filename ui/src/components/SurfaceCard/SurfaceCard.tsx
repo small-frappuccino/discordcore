02d30f0fbefe214773602adc47e3c4c6c775b2c5
small-frappuccino <QdmyEep2DQMA5f@proton.me>
2026-06-05 01:24:51 -0300

    refactor(ui): update Button and SurfaceCard components

--- ui/src/components/SurfaceCard/SurfaceCard.tsx
+++ ui/src/components/SurfaceCard/SurfaceCard.tsx
@@ -1,13 +1,12 @@
 import * as React from "react";
 
-export function SurfaceCard({
-  className = "",
-  children,
-  ...props
-}: React.HTMLAttributes<HTMLDivElement>) {
-  return (
-    <div className={`surface-card ${className}`} {...props}>
-      {children}
-    </div>
-  );
-}
+export const SurfaceCard = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
+  ({ className = "", children, ...props }, ref) => {
+    return (
+      <div ref={ref} className={`surface-card ${className}`} {...props}>
+        {children}
+      </div>
+    );
+  }
+);
+SurfaceCard.displayName = "SurfaceCard";
